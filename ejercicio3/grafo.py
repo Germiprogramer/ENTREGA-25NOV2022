f2ebaeb32478aaa264cf469ec6bcc7bab3849ff5
Germiprogramer <llorentemunozgerman@gmail.com>
2022-11-24 00:17:48 +0100

    a

---
+++ ejercicio3/grafo.py
@@ -0,0 +1,102 @@
+class Maravilla():
+    def __init__(self, nombre, pais, tipo):
+        self.nombre = nombre
+        self.pais = pais
+        self.tipo = tipo
+
+#creamos las maravillas, cada una de ellas será un vértice
+
+estatuadeZeus = Maravilla("Estatua de Zeus", "Grecia", "Natural")
+templodeArtemisa = Maravilla("Templo de Artemisa", "Turquia", "Natural")
+mausoleo_halicarnaso = Maravilla("Mausoleo Halicarnaso", "Turquia", "Natural")
+jardines_colgantes = Maravilla("Jardines colgantes", "Irak", "Natural")
+coloso_rodas = Maravilla("Coloso de Rodas", "Grecia", "Natural")
+faro_alejandria = Maravilla("Faro de Alejandría", "Egipto", "Natural")
+piramide = Maravilla("Piramide de Guiza", "Egipto", "Natural")
+
+class nodoVertice():
+    def __init__(self, info):
+        self.info = info
+        self.visitado = False
+        self.sig = None #aqui ira un nodoarista
+        self.adyacentes = lista_Aristas()
+
+class nodoArista():
+    def __init__(self, distancia, destino):
+        self.distancia = distancia
+        self.destino = destino
+        self.sig = None
+
+class lista_Aristas():
+    def __init__(self):
+        self.aristas= []
+        self.tamanio = len(self.aristas)
+    def printear_aristas(self):
+        for i in self.aristas:
+            print(f"la distancia a {i.destino.info.nombre} es {i.distancia}")
+
+
+
+class Grafo():
+    def __init__(self, dirigido = True):
+        self.vertices = []
+        self.dirigido = dirigido
+        self.tamanio = 0
+
+def insertar_vertice(grafo, dato):
+    #hacemos que la clase Maravilla sea el dato
+    aux = nodoVertice(dato)
+    dato = aux
+    grafo.vertices.append(dato)
+
+def insertar_arista(grafo, dato, origen, destino):
+    o = False
+    d = False
+    for i in grafo.vertices:
+        if origen == i.info.nombre:
+            o = True 
+            origen = i
+        else: 
+            pass
+        if destino == i.info.nombre:
+            d = True
+            destino = i
+        else: 
+            pass
+    if o is True and d is True:
+        agregar_arista(origen, dato, destino)
+        if not grafo.dirigido:
+            agregar_arista(destino, dato, origen)
+    else:
+        print("Uno de los dos vértices no está en el grafo")
+
+def agregar_arista(origen, dato, destino):
+    nodo = nodoArista(dato, destino)
+    origen.adyacentes.aristas.append(nodo)
+
+#creacion del grafo
+
+grafo = Grafo(False)
+
+insertar_vertice(grafo, estatuadeZeus)
+insertar_vertice(grafo, templodeArtemisa)
+insertar_vertice(grafo, mausoleo_halicarnaso)
+insertar_vertice(grafo, jardines_colgantes)
+insertar_vertice(grafo, coloso_rodas)
+insertar_vertice(grafo, faro_alejandria)
+insertar_vertice(grafo, piramide)
+
+insertar_arista(grafo, 1000, "Estatua de Zeus", "Templo de Artemisa")
+insertar_arista(grafo, 2000, "Estatua de Zeus", "Mausoleo Halicarnaso")
+
+
+
+
+#print(grafo.vertices[0].adyacentes)
+#grafo.vertices[0].adyacentes.printear_aristas()
+
+
+
+
+
+
